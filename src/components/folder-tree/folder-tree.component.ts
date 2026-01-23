a6ed5d55c0cca74f0a898c983b1f8764b5d839c6
arjendebruijn <a.w.debruijn@gmail.com>
2026-01-23 16:16:13 +0100

    Starting with setting and updating checkbox states and getting selected items

--- src/components/folder-tree/folder-tree.component.ts
+++ src/components/folder-tree/folder-tree.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { TreeNode } from '../../models/types';
 import { NgTemplateOutlet } from '@angular/common';
 
@@ -10,5 +10,48 @@ import { NgTemplateOutlet } from '@angular/common';
   imports: [NgTemplateOutlet],
 })
 export class FolderTreeComponent {
-  folderTree = input<TreeNode | null>();
+  folderTree = input.required<TreeNode>();
+
+  selectedItemIds = signal<number[]>([]);
+  selectedFolderIds = signal<number[]>([]);
+
+  constructor() {
+    effect(() => console.log('selected folder ids:', this.selectedFolderIds()));
+    effect(() => console.log('selected item ids:', this.selectedItemIds()));
+  }
+
+  isIndeterminate(children: TreeNode[]) {
+    const childrenIds = children.map((child) => child.id);
+    const someSelected = this.selectedItemIds().some((itemId) =>
+      childrenIds.find((id) => id === itemId),
+    );
+
+    const allSelected = this.selectedItemIds().every((itemId) =>
+      childrenIds.find((id) => id === itemId),
+    );
+
+    return allSelected ? false : someSelected;
+  }
+
+  toggleFolderSelected(id: number) {
+    const selectedIds = this.selectedFolderIds();
+    if (selectedIds.some((selectedId) => selectedId === id)) {
+      const filtered = selectedIds.filter((selectedId) => selectedId !== id);
+      this.selectedFolderIds.set(filtered);
+      return;
+    }
+
+    this.selectedFolderIds.update((ids) => [...ids, id]);
+  }
+
+  toggleItemSelected(id: number) {
+    const selectedItemIds = this.selectedItemIds();
+    if (selectedItemIds.some((selectedId) => selectedId === id)) {
+      const filtered = selectedItemIds.filter((selectedId) => selectedId !== id);
+      this.selectedItemIds.set(filtered);
+      return;
+    }
+
+    this.selectedItemIds.update((ids) => [...ids, id]);
+  }
 }
